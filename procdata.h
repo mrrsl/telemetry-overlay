b37c8efdbafd57b25a3ee38f50b2ca1d123c0eb5
Morris L <morrislli@outlook>
2026-01-27 09:30:29 -0800

    const correctness

--- procdata.h
+++ procdata.h
@@ -27,7 +27,7 @@ class ProcData {
     static constexpr int MICROSEC_TO_FILETIME =  10;
 
     /** Resource name to use for `IWebmLocator::ConnectServer`. */
-    static constexpr wchar_t*  WMI_RESOURCE_NAME = L"ROOT\\CIMV2";
+    static const wchar_t*  WMI_RESOURCE_NAME = L"ROOT\\CIMV2";
 
     /** Set to true if there were no errors during initialization. */
     bool initSuccess;
